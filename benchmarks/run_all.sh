0f1da5fb49d4e1161bf7c7d240214f487250a606
t-k- <clock126@126.com>
2021-12-18 23:10:47 -0500

    update run_all.sh

--- benchmarks/run_all.sh
+++ benchmarks/run_all.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 set -e
 > config.csv
+
+skip=false
+skip_until='DIN'
 for path in $(find ../config/model_config -name '*.yaml'); do
     filename=$(basename $path)
     if [[ $filename == tests.yaml || $filename == common.yaml ]]; then
         continue
     fi
     model_name=$(echo $filename | cut -d . -f1)
-    if [[ $model_name == HFM ]]; then
+    echo ${model_name}
+
+    if [[ $model_name == $skip_until ]]; then
+        skip=false
+    fi
+    if [[ $model_name == HFM || $model_name == LorentzFM || $model_name == FLEN || $model_name == DIN ]]; then
         continue
     fi
-    echo ${model_name}
-    python run_expid.py --expid ${model_name}_base
+
+    if [[ $skip == false ]]; then
+        python run_expid.py --expid ${model_name}_base
+    fi
 done
