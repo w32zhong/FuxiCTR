9a6a42d227324c5602ad222e6e63a1d990221719
t-k- <clock126@126.com>
2022-01-25 04:50:03 -0500

    update benchmarks/rank_marginal_prepare.sh

--- benchmarks/rank_marginal_prepare.sh
+++ benchmarks/rank_marginal_prepare.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 ### Sample ###
- cat ../data/mathclicks/test.csv.retrieval | sort -R | head -n 100 > ../data/mathclicks/test.sample.csv
+cat ../data/mathclicks/test.retrieval.csv | sort -R | head -n 100 > ../data/mathclicks/test.sample.csv
 
 ### Generate rank-marginal features ###
-head -n 1 ../data/mathclicks/test.csv.retrieval > ../data/mathclicks/test.csv
+head -n 1 ../data/mathclicks/test.retrieval.csv > ../data/mathclicks/test.csv
 while read line; do
 	for i in {1..100}; do
 		echo $line | awk -F, "{OFS=FS}{\$5 = $i; print}"
 	done >> ../data/mathclicks/test.csv
 done < ../data/mathclicks/test.sample.csv
+
+### Last step...
+echo "Please activate the (commented) RERANK handler in fuxictr/metrics.py"
