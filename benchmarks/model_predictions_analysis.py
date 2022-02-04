dcf9f5eac1438784a2548c7ce583b5cc36c92be7
t-k- <clock126@126.com>
2022-02-04 15:50:43 -0500

    being able to calc cond_mi and feature forward selection

--- benchmarks/model_predictions_analysis.py
+++ benchmarks/model_predictions_analysis.py
@@ -6,23 +6,84 @@ import pickle
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.feature_selection import mutual_info_regression
+from sklearn.neighbors import KDTree
+from scipy.special import digamma
 
 fields = 'a0_sym,a0_stct,a0_score,token_sim,deep_f_sim,deep_tag_sim,f_len_q,f_len_d'
 fields = fields.split(',')
 
-def mut_info_analysis(csv_file, n_rows=40_000):
+# Reference: https://github.com/gregversteeg/NPEET/blob/master/npeet/entropy_estimators.py
+
+# UTILITY FUNCTIONS
+def add_noise(x, intens=1e-10):
+    return x + intens * np.random.random_sample(x.shape)
+def query_neighbors(tree, x, k):
+    return tree.query(x, k=k + 1)[0][:, k]
+def count_neighbors(tree, x, r):
+    return tree.query_radius(x, r, count_only=True)
+def avgdigamma(points, dvec):
+    # This part finds number of neighbors in some radius in the marginal space
+    # returns expectation value of <psi(nx)>
+    tree = build_tree(points)
+    dvec = dvec - 1e-15
+    num_points = count_neighbors(tree, points, dvec)
+    return np.mean(digamma(num_points))
+def build_tree(points):
+    return KDTree(points, metric='chebyshev')
+
+# IM functions
+def entropy(x, k=3, base=2):
+    """ The classic K-L k-nearest neighbor continuous entropy estimator
+        x should be a list of vectors, e.g. x = [[1.3], [3.7], [5.1], [2.4]]
+        if x is a one-dimensional scalar and we have four samples
+    """
+    assert k <= len(x) - 1, "Set k smaller than num. samples - 1"
+    x = np.asarray(x)
+    n_elements, n_features = x.shape
+    x = add_noise(x)
+    tree = build_tree(x)
+    nn = query_neighbors(tree, x, k)
+    const = digamma(n_elements) - digamma(k) + n_features * np.log(2)
+    return (const + n_features * np.log(nn).mean()) / np.log(base)
+
+
+def cond_entropy(X, Y):
+    XY = np.hstack((X, Y))
+    return entropy(XY) - entropy(Y)
+
+
+def mutual_info(X, Y):
+    #return entropy(X) - cond_entropy(X, Y)
+    return entropy(Y) - cond_entropy(Y, X)
+
+
+def cond_mutual_info(X, Y, Z):
+    if len(Z) == 0:
+        return mutual_info(X, Y)
+    YZ = np.hstack((Y, Z))
+    return mutual_info(X, YZ) - mutual_info(X, Z)
+
+
+def mut_info_analysis(csv_file, n_rows=40_000, top_k_feat=3):
     df = pd.read_csv(csv_file)
     print('Data rows:', len(df))
     print('Sample rows:', n_rows)
     df = df[:n_rows]
-
     global fields
-    X = df[fields].to_numpy()
-    y = df['y_pred'].to_numpy()
 
-    mut_info = mutual_info_regression(X, y)
-    return mut_info
+    Y = df['y_pred'].to_numpy().reshape(-1, 1)
+    Z_set = []
+    for k in range(top_k_feat):
+        field_cmi = []
+        for field in fields:
+            X = df[field].to_numpy().reshape(-1, 1)
+            Z = np.asarray(Z_set).T
+            cmi = cond_mutual_info(Y, X, Z)
+            field_cmi.append(cmi)
+        max_field = fields[np.asarray(field_cmi).argmax()]
+        max_cmi = max(field_cmi)
+        print(f'top-{k} max field:', max_field, '=', max_cmi)
+        Z_set.append(df[max_field].to_numpy())
 
 
 def get_all_mut_info(root_dir):
